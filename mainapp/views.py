e75d1d3544324aa3c6a1c6881265d702a93a0b08
ram1k <zo0zo0@mail.ru>
2021-09-05 12:12:09 +0300

    Added Homework 2 and 3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from mainapp.models import Product
 # Create your views here.
+
+
 def main(requests):
+    '''title = 'Главная'
+    products = Product.object.all()
+    content = {"title": title, 'products': products}'''
     return render(requests, 'mainapp/index.html')
 
 def products(requests):
